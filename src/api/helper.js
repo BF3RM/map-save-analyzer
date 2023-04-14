403de017e5f509d44ea9b21a61f8684466b70fdd
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2023-04-14 14:50:34 -0400

    add getDirectories and getFile api calls

--- src/api/helper.js
+++ src/api/helper.js
@@ -4,8 +4,23 @@ const testingURL = "http://localhost:5000/api"
 const baseURL = "/api"
 
 export async function postFile(formData) {
-    return axios.post(`${testingURL}/file`, formData)
+    return axios.post(`${testingURL}/newFile`, formData)
         .then(response => {
             return response?.data
     }).catch((error) => console.error(error))
+}
+
+export async function getDirectories() {
+    return axios.get(`${testingURL}/directories`)
+        .then(response => {
+            return response?.data
+    }).catch((error) => console.error(error))
+}
+
+export async function getFile(dataObject) {
+    console.log(dataObject)
+    return axios.post(`${testingURL}/getFile`, dataObject)
+        .then(response => {
+            return response?.data
+        }).catch((error) => console.error(error))
 }
