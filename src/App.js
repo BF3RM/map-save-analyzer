8649f635001ffef45ec12ea9967423136bef649c
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2023-04-14 01:52:14 -0400

    add sorting, checkbox logic
    can flag items for removal. EX: Trees :D

--- src/App.js
+++ src/App.js
@@ -6,18 +6,71 @@ export default function App() {
     const [details, setDetails] = React.useState(null)
     const [spawnedItems, setSpawnedItems] = React.useState(null)
     const [nameFilter, setNameFilter] = React.useState(null) 
+    const [objectsToRemove, setObjectsToRemove] = React.useState([])
 
     function showUploadedFile(payload) {
         setDetails(() => payload)
         setSpawnedItems(() => payload.spawnedItems)
     }
 
-    function sortSpawnedItems(){
-
+    function sortSpawnedItems(e){
+        // 25b2 is UP
+        // 25BC is DOWN
+        if(e.target.innerText === "\u25BC")
+        {
+            e.target.innerText = "\u25B2"
+            let tempArr = [...spawnedItems].sort((a, b) => {
+                if (a.count > b.count) {
+                    return 1
+                }
+                if (a.count < b.count) {
+                    return -1
+                }
+                return 0
+            })
+            setSpawnedItems(() => tempArr)
+        } else {
+            e.target.innerText = "\u25BC"
+            let tempArr = [...spawnedItems].sort((a, b) => {
+                if (a.count < b.count) {
+                    return 1
+                }
+                if (a.count > b.count) {
+                    return -1
+                }
+                return 0
+            })
+            setSpawnedItems(() => tempArr)
+        }
+        
     }
 
     function handleSearchChange(e) {
         setNameFilter(() => e.target.value)
+    }
+
+    function handleCheckBox(name) {
+        let arr = [...objectsToRemove]
+        let index = arr.findIndex((item) => item === name)
+        if(index === -1){
+            arr.push(name)
+            setObjectsToRemove(() => [...arr])
+        } else {
+            arr = arr.filter((item, curIndex) => {
+                if(curIndex !== index ) {
+                    return item
+                }
+            })
+            setObjectsToRemove(() => [...arr])
+        }
+    }
+
+    function handleRemoveObjects() {
+        // axios post to server
+        // update details, and spawnedItems.
+        console.table(objectsToRemove)
+        const continueDelete = window.confirm(`Are you sure? This will delete: \n\n${objectsToRemove.join("\n\n")}`)
+        console.log(continueDelete)
     }
 
     React.useEffect(() => {
@@ -37,9 +90,8 @@ export default function App() {
             }
             {
                 details && <>
-                    <h1>Details</h1>
-                    <div>
-                        <h2>Project name: {details.projectName}</h2>
+                    <div className="heading">
+                        <h2 className="example" id="myId">{details.projectName}</h2>
                         <p>Map: {details.mapName}</p>
                         <p>Game Mode:  {details.gameModeName}</p>
                         <p>Save version: {details.saveVersion}</p>
@@ -47,13 +99,13 @@ export default function App() {
                     
                     <table className="spawnedItemsTable">
                         <caption>
-                            FILTER RESULTS: <input type="text" onChange={handleSearchChange} value={nameFilter}></input>
+                            FILTER RESULTS: <input type="text" onChange={handleSearchChange} value={nameFilter}></input> {objectsToRemove.length >= 1 && <button onClick={handleRemoveObjects}>DELETE SELECTED</button>}
                         </caption>
                         <thead>
                             <tr>
                             <th scope="col">Selected</th>
                             <th scope="col">Item Name</th>
-                            <th scope="col">Amount</th>
+                            <th scope="col">Amount <button onClick={sortSpawnedItems}>{`\u25BC`}</button></th>
                             <th scope="col">GUID</th>
                             </tr>
                         </thead>
@@ -61,7 +113,7 @@ export default function App() {
                             {spawnedItems.map((item, index) => {
                                 return (
                                     <tr key={index}>
-                                        <input className="checkBoxes" type="checkbox" />
+                                        <td><input className="checkBoxes" type="checkbox" onClick={() => {handleCheckBox(item.name)}} /></td>
                                         <td className="itemName" key={index + "name"}>
                                             {item.name}
                                             </td>
